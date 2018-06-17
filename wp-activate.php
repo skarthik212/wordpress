9c46f2c8321ff4b4e2ca5c3c56e969804b430d64
skarthik212 <karthikeyan3512@gmail.com>
2018-06-17 16:14:18 +0530

    COmmit Changes

--- wp-activate.php
+++ wp-activate.php
@@ -11,6 +11,9 @@ define( 'WP_INSTALLING', true );
 echo "Test Check";
 
 /** Sets up the WordPress Environment. */
+
+
+
 require( dirname(__FILE__) . '/wp-load.php' );
 
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
